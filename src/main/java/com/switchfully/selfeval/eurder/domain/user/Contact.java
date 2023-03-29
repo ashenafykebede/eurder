771eb31677f5ccf649ab3e1fc70a3ff4d7ab4972
Ashenafi Kebede <ashuewa@gmail.com>
2023-03-29 10:40:00 +0200

    Tests for contact class added

---
+++ src/main/java/com/switchfully/selfeval/eurder/domain/user/Contact.java
@@ -0,0 +1,19 @@
+package com.switchfully.selfeval.eurder.domain.user;
+
+public class Contact {
+    private String email;
+    private String phoneNumber;
+
+    public Contact(String email, String phoneNumber) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return null;
+    }
+
+    public String getPhoneNumber() {
+        return null;
+    }
+}
