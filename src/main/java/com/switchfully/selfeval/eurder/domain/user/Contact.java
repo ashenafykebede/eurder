9cd65515a80435e685e3f4e9ce29c83646e4741a
Ashenafi Kebede <ashuewa@gmail.com>
2023-03-29 11:07:47 +0200

    Contact class implemented(tests are green)

--- src/main/java/com/switchfully/selfeval/eurder/domain/user/Contact.java
+++ src/main/java/com/switchfully/selfeval/eurder/domain/user/Contact.java
@@ -5,15 +5,29 @@ public class Contact {
     private String phoneNumber;
 
     public Contact(String email, String phoneNumber) {
-        this.email = email;
-        this.phoneNumber = phoneNumber;
+        this.email = validateEmail(email);
+        this.phoneNumber = validatePhoneNumber(phoneNumber);
+    }
+
+    private String validatePhoneNumber(String phoneNumber) {
+        if ( phoneNumber==null || phoneNumber.isEmpty()){
+            throw new IllegalArgumentException("Incorrect input");
+        }
+        return phoneNumber;
+    }
+
+    private String validateEmail(String email) {
+        if ( email==null || email.isEmpty() || !email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")){
+            throw new IllegalArgumentException("Incorrect input");
+        }
+        return email;
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 
     public String getPhoneNumber() {
-        return null;
+        return phoneNumber;
     }
 }
